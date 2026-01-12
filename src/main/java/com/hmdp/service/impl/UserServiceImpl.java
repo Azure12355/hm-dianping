0ec1dc6ce338e4d7de12c1623e3416c5861f6376
lytton <azure12355@gmail.com>
2026-01-12 12:42:57 +0800

    feat: 完成发送短信验证码的功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import io.netty.util.internal.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -14,7 +21,26 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        // 2.生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 3.保存验证码到 session 中
+        session.setAttribute("code", code);
+
+        // 4.发送验证码(模拟发送验证码)
+        log.info("发送验证码成功，验证码为：{}", code);
+
+        // 5.返回响应
+        return Result.ok("验证码发送成功");
+    }
 }
